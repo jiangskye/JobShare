c7fe18f8ab77ab45b565ba6bf841ff0099b1da65
Vicentina <32057244+Vicentina@users.noreply.github.com>
2020-04-25 22:07:58 +0800

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -1,110 +1,51 @@
-// var express = require('express');
-// var router = express.Router();
-// const bcrypt = require('bcryptjs');
-// const mongoose = require('mongoose');
-// const passportLocalMongoose = require('passport-local-mongoose');
-// const passport = require('passport');
-// const connectEnsureLogin = require('connect-ensure-login');
-//
-//
-// //schema
-// // const Schema = mongoose.Schema;
-// // const UserDetail = new Schema({
-// //     username: String,
-// //     password: String
-// // });
-//
-//
-// //auth
-// var dbHost = 'localhost';
-// var dbPort = 27017;
-// // var dbName = 'jobshare';
-// // mongoose.connect('mongodb://' + dbHost + ':' + dbPort + '/' + dbName,
-// //     { useNewUrlParser: true, useUnifiedTopology: true });
-// // UserDetail.plugin(passportLocalMongoose);
-// //const UserDetails = mongoose.model('userInfo', UserDetail, 'userInfo');
-//
-// /* PASSPORT LOCAL AUTHENTICATION */
-// // passport.use(UserDetails.createStrategy());
-// // passport.serializeUser(UserDetails.serializeUser());
-// // passport.deserializeUser(UserDetails.deserializeUser());
-//
-//
-// /* GET users listing. */
-//
-//
-// router.get('/login', function(req, res, next) {
-//     res.render('login');
-// });
-//
-// router.get('/register', function(req, res, next) {
-//     res.render('register');
-// });
-//
-// router.post('/register', function(req, res, next){
-//     const name = req.body.name;
-//     const email = req.body.email;
-//     const psw = req.body.psw;
-//     const psw2 = req.body.psw2;
-//
-//     req.checkbody('name', 'Name is required.').notEmpty();
-//     req.checkbody('email', 'email is not valid.').isEmail();
-//     req.checkbody('psw', 'password is required.').notEmpty();
-//     req.checkbody('psw2', 'confirm password do not match.').equals(req.body.psw);
-//
-//     let errors = req.validationErrors();
-//     if (errors){
-//         res.render('register', {
-//             errors: errors
-//         })
-//     } else{
-//         let newUser = new User({
-//             name: name,
-//             email: email,
-//             password: password
-//         });
-//         bcrypt.getSalt(10, function(err, salt){
-//             bcrypt.hash(newUser.password, salt, function(err, hash){
-//                 if (err){
-//                     console.log(err);
-//                 }
-//                 newUser.password = hash;
-//                 newUser.save()
-//             });
-//         })
-//     }
-// });
-//
-// router.get('/user',
-//     connectEnsureLogin.ensureLoggedIn(),
-//     (req, res) => res.send({user: req.user})
-// );
-// router.get('/',
-//     connectEnsureLogin.ensureLoggedIn(),
-//     (req, res) => res.render()
-// );
-// router.post('/login', (req, res, next) => {
-//     passport.authenticate('local',
-//         (err, user, info) => {
-//             if (err) {
-//                 return next(err);
-//             }
-//             if (!user) {
-//                 return res.redirect('/login?info=' + info);
-//             }
-//             req.logIn(user, function(err) {
-//                 if (err) {
-//                     return next(err);
-//                 }
-//                 return res.redirect('/');
-//             });
-//         })(req, res, next);
-// });
-// module.exports = router;
-
-
 var express = require('express');
 var router = express.Router();
+const bcrypt = require('bcryptjs');
+const mongoose = require('mongoose');
+const passport = require('passport');
+require('../config/passport')(passport);
+
+
+//auth
+var dbHost = 'localhost';
+var dbPort = 27017;
+var dbName = 'jobshare';
+mongoose.connect('mongodb://' + dbHost + ':' + dbPort + '/' + dbName,
+    { useNewUrlParser: true, useUnifiedTopology: true });
+
+
+/*log in and register */
+router.get('/login', function(req, res, next) {
+    res.render('login');
+});
+
+router.post(
+    "/login",
+    passport.authenticate("local-login", {
+        successRedirect: "/users/userprofile",
+        failureRedirect: "/users/login",
+        failureFlash: true,
+    })
+);
+
+router.get('/register', function(req, res, next) {
+    res.render('register');
+});
+
+router.post('/register', passport.authenticate('local-signup', {
+    successRedirect : '/users/userprofile', // redirect to the secure profile section
+    failureRedirect : '/users/register', // redirect back to the signup page if there is an error
+    failureFlash : true // allow flash messages
+}));
+
+
+router.get('/userprofile',isLoggedIn,(req,res)=>{
+    console.log("req user",req.user);
+    console.log("profile");
+    res.render('userprofile',{
+        user : req.user
+    });
+});
 
 var PubContent = require("../models/Pubcontent");
 
@@ -126,5 +67,25 @@ router.post('/PubContent/add',function (req,res) {
     })
 
 });
+
+router.get('/logout', function(req, res) {
+    req.logout();
+    res.redirect('/');
+});
+
+function isLoggedIn(req, res, next) {
+    // if user is authenticated in the session, carry on
+    console.log(req.session);
+    console.log(req.user);
+    console.log(req.isAuthenticated());
+    if (req.isAuthenticated()){
+        console.log("req.user.email");
+        console.log("authenticated");
+        return next();}
+
+    // if they aren't redirect them to the home page
+    console.log("not log in");
+    res.redirect('/users/login');
+}
 
 module.exports = router;
