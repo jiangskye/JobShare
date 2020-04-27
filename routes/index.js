265cad449305331ab68e250fe29c8b0d25bd7510
jiangskye <51129569+jiangskye@users.noreply.github.com>
2020-04-27 12:03:48 +0800

    Final change version

--- routes/index.js
+++ routes/index.js
@@ -17,10 +17,42 @@ mongoose.connect('mongodb://' + dbHost + ':' + dbPort + '/' + dbName,
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    res.render('main/index', { title: 'Express' });
 });
 
+router.get('/employeesharing', function(req, res, next) {
+    res.render('main/employee-sharing', { title: 'Express' });
+});
 
+router.get('/jobrecruitment', function(req, res, next) {
+    res.render('main/job-recruitment', { title: 'Express' });
+});
 
+router.get('/availableindi', function(req, res, next) {
+    res.render('main/available-individual', { title: 'Express' });
+});
 
+router.get('/availabletaskdoer', function(req, res, next) {
+    res.render('main/available-task-doer', { title: 'Express' });
+});
+
+router.get('/publishedtask', function(req, res, next) {
+    res.render('main/published-tasks', { title: 'Express' });
+});
+
+router.get('/recruitingcompany', function(req, res, next) {
+    res.render('main/recruiting-company', { title: 'Express' });
+});
+router.get('/shorttermborrow', function(req, res, next) {
+    res.render('main/short-term-borrowing', { title: 'Express' });
+});
+router.get('/shorttermlend', function(req, res, next) {
+    res.render('main/short-term-lending', { title: 'Express' });
+});
+router.get('/taskfinding', function(req, res, next) {
+    res.render('main/task-finding', { title: 'Express' });
+});
+router.get('/contact', function(req, res, next) {
+    res.render('main/contactus', { title: 'Express' });
+});
 module.exports = router;
